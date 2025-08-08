09427bccad42bf4eed4ccd986e24f6c97fbbfbe4
SanjuSreejith <sanjusreejithu@gmail.com>
2025-08-09 05:14:40 +0530

    Add files via upload

---
+++ ScoreManageer.cs
@@ -0,0 +1,24 @@
+// Create a new script called MenuScoreDisplay.cs
+using TMPro;
+using UnityEngine;
+
+public class MenuScoreDisplay : MonoBehaviour
+{
+    public TMP_Text scoreText;
+
+    void Start()
+    {
+        // Load the saved score
+        int savedScore = PlayerPrefs.GetInt("PlayerScore", 0);
+
+        // Display it
+        scoreText.text = "Your Score: " + savedScore;
+    }
+
+    // Optional: Add a method to reset the score
+    public void ResetScore()
+    {
+        PlayerPrefs.SetInt("PlayerScore", 0);
+        scoreText.text = "Your Score: 0";
+    }
+}
